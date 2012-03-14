452dd62fe5ce88ec0e7148a1500a8fd46c6d97d7
Themis Savvidis <themhs@gmail.com>
2012-03-14 14:11:09 +0200

    remove empty line

--- transifex/teams/signals.py
+++ transifex/teams/signals.py
@@ -7,5 +7,4 @@ team_join_approved = Signal(providing_args=[
 team_join_denied = Signal(providing_args=[
     'nt', 'context', 'project', 'team', 'access_request', 'error_msg',
 ])
-
 team_member_removed = Signal(providing_args=['user', 'team', 'error_msg'])
