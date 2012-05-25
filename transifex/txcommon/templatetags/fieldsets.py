3d9a8b098226568db64d487f1c1b3b38d73dee05
Konstantinos Bairaktaris <kbairak@transifex.com>
2012-05-25 12:26:51 +0300

    initial commit for dual_form templatetag
    introduced new templatetag called 'dual_form'. It's supposed to be used
    like this:
    
    {% dual_form field_a,field_b[,...] from form as left|right|fullrow %}
    
    This templatetag will be replaced with a list of divs for each form.
    Each div will be decorated with a class name depending on the last
    argument, which will make the field align to the left or right column or
    to be a fullrow field.
    
    Additional features are that helptexts are displayed using tipsy.js,
    required fields' labels have an asterisk (*) appended, error messages
    are displayed properly and fields with validation errors are decorated
    with the 'tx-form-error' class name.

--- transifex/txcommon/templatetags/fieldsets.py
+++ transifex/txcommon/templatetags/fieldsets.py
@@ -1,6 +1,8 @@
 import copy
-from django import template
 from django import forms
+from django import template
+from django.template import Context
+from django.template.loader import get_template
 from django.utils.datastructures import SortedDict
 
 register = template.Library()
@@ -41,3 +43,63 @@ def get_fieldset(parser, token):
 
     return FieldsetNode(fields.split(','), variable_name, form)
 
+@register.tag(name="dual_form")
+def dual_form(parser, token):
+    """
+        Template tag used to help with forms that appear in twin columns.
+
+        usage: {% dual_form FIELD[,FIELD] from FORM as POSITION %}
+
+        Replaces the templatetag with a list of divs that contain the fields
+        along  with the labels, error messages, etc.
+        Fields with error messages are decorated with the 'tx-form-error' class
+
+        example:
+        <form action="" method="post">
+            {% dual_form first_name,location,blog,about from form as left %}
+            {% dual_form last_name,languages,linkedin,twitter,looking_for_work from form as right %}
+            {% dual_form looking_for_work from form as fullrow %}
+            <div class="submit-button">
+                <input type="submit" value="{% trans "Save changes" %}" />
+            </div>
+        </form>
+    """
+
+    contents = token.split_contents()
+    try:
+        name, fields, from_, form, as_, position = contents
+    except ValueError, e:
+        raise template.TemplateSyntaxError(
+            "Bad arguments for %s, "\
+            "usage: %s FIELD[,FIELD] from FORM as POSITION" % contents[0]
+        )
+
+    return DualFormNode(fields.split(','), form, position)
+
+class DualFormNode(template.Node):
+    def __init__(self, fields, form_variable, position):
+        if position not in ('left', 'right', 'fullrow', ):
+            raise template.TemplateSyntaxError(
+                "usage: %s FIELD[,FIELD] from FORM as POSITION, "\
+                "valid values for POSITION are: 'left', 'right' and 'fullrow'"\
+                % contents[0]
+            )
+        self.position = position
+        self.fields = fields
+        self.form_variable = form_variable
+
+    def render(self, context):
+        form = template.Variable(self.form_variable).resolve(context)
+        new_form = copy.copy(form)
+        new_form.fields = SortedDict(
+            [(key, form.fields[key]) for key in self.fields]
+        )
+
+        class_name = {'left':    "first-tdfc",
+                      'right':   "second-tdfc",
+                      'fullrow': "full-tdfc"}[self.position]
+
+        template_obj = get_template("txcommon/dual_forms.html")
+        context = Context({'form': new_form, 'class_name': class_name})
+
+        return template_obj.render(context)
