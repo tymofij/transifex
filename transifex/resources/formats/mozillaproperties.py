507387287cb1b94751b1331f781e4ec1c3fb8adf
Tim Babych <tim.babych@gmail.com>
2013-04-03 01:35:12 +0300

    do not turn \u0020 into spaces

--- transifex/resources/formats/mozillaproperties.py
+++ transifex/resources/formats/mozillaproperties.py
@@ -41,22 +41,38 @@ class MozillaPropertiesHandler(FillEmptyCompilerFactory, PropertiesHandler):
             r'\\[uU]([0-9A-Fa-f]{4})')
 
     def _escape(self, s):
-        return (s.replace('\\', r'\\')
-                 .replace('\n', r'\n')
-                 .replace('\r', r'\r')
-                 .replace('\t', r'\t')
+        """ preparing the string to be written to file
+        """
+        s = (s.replace('\n', r'\n')
+              .replace('\r', r'\r')
+              .replace('\t', r'\t')
         )
+        # double the slashes, except when they start unicode sequence
+        # or other control character
+        return re.sub(r'\\([^uUnrt$])', r'\\\\\1', s)
 
     def _unescape(self, s):
+        """ Outputting the strings to the screen
+        """
         return (s.replace(r'\n', '\n')
                  .replace(r'\r', '\r')
                  .replace(r'\t', '\t')
                  .replace(r'\\', '\\')
         )
 
+
     def _visit_value(self, value):
+
+        def unpattern(m):
+            """ Convert \uXXXX into its unicode equivalent.
+                Except for spaces. \u0020 are there probably for a reason,
+                and that reason usually is that Moz parser strips trailing spaces,
+                so the only mean to use them is to put \u0020 into string.
+            """
+            charcode = m.group(1)
+            if charcode == '0020':
+                return r'\u'+charcode
+            return unichr(int(charcode, 16))
+
         if value:
-            return self.escaped_unicode_pattern.sub(
-                    lambda m: unichr(int(m.group(1), 16)),
-                    value
-            )
+            return self.escaped_unicode_pattern.sub(unpattern, value)
